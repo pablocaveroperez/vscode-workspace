f347e67695139d4e5ce105bff85810f25469ccd6
Pablo Cavero Perez <gelves.mula@gmail.com>
2021-01-23 22:02:59 +0100

    Terminado el EJ2

--- Tests/EJ2.py
+++ Tests/EJ2.py
@@ -2,6 +2,37 @@
 print("Modelos de presupuestos")
 
 class ModeloDePresupuesto:
+    # Datos comerciales
+    
+
+    @property
+    def titulo(self):
+        return self.titulo
+    @titulo.setter
+    def titulo(self, titulo):
+        self.titulo = titulo
+
+    @property
+    def encabezado_nombre(self):
+        return self.encabezado_nombre
+    @encabezado_nombre.setter
+    def encabezado_nombre(self, encabezado_nombre):
+        self.encabezado_nombre = encabezado_nombre
+
+    @property
+    def encabezado_web(self):
+        return self.encabezado_web
+    @encabezado_web.setter
+    def encabezado_web(self, encabezado_web):
+        self.encabezado_web = encabezado_web
+
+    @property
+    def encabezado_email(self):
+        return self.encabezado_email
+    @encabezado_email.setter
+    def encabezado_email(self, encabezado_email):
+        self.encabezado_email = encabezado_email
+
     @property
     def cliente(self):
         return self.cliente
@@ -17,14 +48,55 @@ class ModeloDePresupuesto:
         self.empresa = empresa
 
     @property
-    def 
+    def fecha(self):
+        return self.fecha
+    @fecha.setter
+    def fecha(self, fecha):
+        self.fecha = fecha
 
-    # Datos comerciales
+    @property
+    def servicio(self):
+        return self.servicio
+    @servicio.setter
+    def servicio(self, servicio):
+        self.servicio = servicio
+
+    @property
+    def importe(self):
+        return self.importe
+    @importe.setter
+    def importe(self, importe):
+        self.importe = importe
+
+    @property
+    def vencimiento(self):
+        return self.vencimiento
+    @vencimiento.setter
+    def vencimiento(self, vencimiento):
+        self.vencimiento = vencimiento
+
+    @property
+    def monto_iva(self):
+        return self.monto_iva
+
+    @property
+    def divline(self):
+        return self.divline
+
+    @property
+    def IVA(self):
+        return self.IVA
+
+    @property
+    def neto(self):
+        return self.neto
+
     titulo = "PRESUPUESTO"
     encabezado_nombre = "Eugenia Bahit"
     encabezado_web = "www.eugeniabahit.com.ar"
     encabezado_email = "mail@mail.com"
 
+    
     # Datos impositivos
     IVA = 21
 
@@ -32,25 +104,26 @@ class ModeloDePresupuesto:
     divline = "="*80
 
     # Setear los datos del cliente
-    def set_cliente(self):
-        self.empresa = input('\tEmpresa: ')
-        self.cliente = Cliente()
-    # Setear los datos basicos ́del presupuesto
+    def set_cliente(self, cliente, empresa):
+        self.empresa = empresa
+        self.cliente = cliente
 
-    def set_datos_basicos(self):
-        self.fecha = input('\tFecha: ')
-        self.servicio = input('\tDescripción del servicio: ')
-        importe = input('\tImporte bruto: €')
-        self.importe = float(importe)
-        self.vencimiento = input('\tFecha de caducidad: ')
+    # Setear los datos basicos ́del presupuesto
+    def set_datos_basicos(self, fecha, servicio, importe, vencimiento):
+        self.fecha = fecha
+        self.servicio = servicio
+        self.importe = importe
+        self.vencimiento = vencimiento
 
     # Calcular IVA
     def calcular_iva(self):
         self.monto_iva = self.importe*self.IVA/100
+        return self.monto_iva
 
     # Calcula el monto total del presupuesto
     def calcular_neto(self):
         self.neto = self.importe+self.monto_iva
+        return self.neto
 
     # Montar el presupuesto
     def montar_presupuesto(self):
@@ -72,24 +145,37 @@ class ModeloDePresupuesto:
         txt += '-'*80
         txt += '\n\tMONTO TOTAL: €%0.2f\n' % (self.neto)
         txt += self.divline+'\n'
-        print(txt)
+        return txt
 
     # Metodo constructor
-    def __init__(self):
+    def __init__(self, cliente, empresa, fecha, servicio, importe, vencimiento):
         print(self.divline)
         print("\tGENERACIÓN DEL PRESUPUESTO")
         print(self.divline)
-        self.set_cliente()
-        self.set_datos_basicos()
+        self.set_cliente(cliente, empresa)
+        self.set_datos_basicos(fecha, servicio, importe, vencimiento)
         self.calcular_iva()
         self.calcular_neto()
         self.montar_presupuesto()
 
 class Cliente:
-    def setCliente(self):
-        self.nombre = input("\nIntroduzca su nombre: ")
-        self.apellidos = input("\nIntroduzca sus apellidos: ")
+    @property
+    def nombre(self):
+        return self.nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        self.nombre = nombre
 
+    @property
+    def apellidos(self):
+        return self.apellidos
+    @apellidos.setter
+    def apellidos(self, apellidos):
+        self.apellidos = apellidos
+
+    def setCliente(self, nombre, apellidos):
+        self.nombre = nombre
+        self.apellidos = apellidos
     # Metodo constructor
-    def __init__(self):
-        self.setCliente()
+    def __init__(self, nombre, apellidos):
+        self.setCliente(nombre, apellidos)
