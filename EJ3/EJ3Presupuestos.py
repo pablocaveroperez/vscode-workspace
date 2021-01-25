8eea031b2c91bb18769223ee1a739abb778912eb
pabcavper <gelves.mula@gmail.com>
2021-01-25 11:46:26 +0100

    Terminado

--- EJ3/EJ3Presupuestos.py
+++ EJ3/EJ3Presupuestos.py
@@ -1,11 +1,23 @@
 from ventas.presupuestos import Cliente, ModeloDePresupuesto
 
-
-cliente = Cliente(input("\nIntroduce el nombre del cliente: "), input("\nIntroduce los apellidos del cliente: "))
+#nombreCliente = input("\nIntroduce el nombre del cliente: ")
+#apellidoCliente = input("\nIntroduce los apellidos del cliente: ")
+nombreCliente = "Pablo"
+apellidoCliente = "Cavero"
+cliente = Cliente()
+cliente.nombre = nombreCliente
+cliente.apellidos = apellidoCliente
 empresa = input("\nIntroduce la empresa: ")
 fecha = input("\nIntroduce la fecha: ")
 servicio = input("\nIntroduce el servicio: ")
 importe = input("\nIntroduce el importe: ")
 vencimiento = input("\nIntroduce el vencimiento: ")
 
+#empresa = "Medac"
+#fecha = "Manaña"
+#servicio = "transporte"
+#importe = 12
+#vencimiento = "Hoy"
+
 modeloDePresupuesto = ModeloDePresupuesto(cliente, empresa, fecha, servicio, importe, vencimiento)
+print(modeloDePresupuesto.montar_presupuesto())
