a51a7fbd6b1d81fd5ad4eb389abd634beb228321
pabcavper <gelves.mula@gmail.com>
2021-01-26 09:57:54 +0100

    Tienda

--- P1/Tienda.py
+++ P1/Tienda.py
@@ -1,3 +1,15 @@
+from P1.Persona import Persona
+
 class Tienda:
-    def __init__(self):
-        pass
+    __personas = []
+    __pedidos = []
+
+    def __init__(self, personas, pedidos):
+        self.__personas = personas
+        self.__pedidos = pedidos
+    
+    def altaCliente(self, cliente):
+        self.__personas.append(cliente)
+
+    def altaVendedor(self, ):
+        
