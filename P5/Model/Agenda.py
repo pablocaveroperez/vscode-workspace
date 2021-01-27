49981e93001ab11df23048a1fb3ee765aa42294f
Pablo Cavero Perez <gelves.mula@gmail.com>
2021-01-27 20:19:38 +0100

    Falta eliminar una entrada de la agenda

--- P5/Model/Agenda.py
+++ P5/Model/Agenda.py
@@ -4,10 +4,10 @@ import linecache
 class Agenda:
 
     def __init__(self):
-        open("file1.txt", "a+")
+        open("file.txt", "a+")
 
     def añadirEntrada(self, oUsuario):
-        archivo = open("file1.txt", "a+")
+        archivo = open("file.txt", "a+")
         lines = archivo.read()
         lines += "\nNombre: {nombre} \nApellido: {apellido} \nTelefono: {telefono}\n "
         archivo.write(lines.format(nombre=oUsuario.nombre, apellido=oUsuario.apellido, telefono=oUsuario.telefono))
@@ -16,15 +16,15 @@ class Agenda:
         x = ""
         contador = 0
 
-        with open("file1.txt", 'r') as read_obj:
+        with open("file.txt", 'r') as read_obj:
             for line in read_obj:
                 contador += 1
                 if "Nombre: " + oUsuario.nombre in line:
 
-                    valor = (linecache.getline("file1.txt", contador + 1))[:-1]
+                    valor = (linecache.getline("file.txt", contador + 1))[:-1]
 
                     if "Apellido: " + oUsuario.apellido == valor:
-                        x = (linecache.getline("file1.txt", contador + 2))
+                        x = (linecache.getline("file.txt", contador + 2))
                         break
 
         return x
