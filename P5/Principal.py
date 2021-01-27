afdcb373bc9637f68c150e83f43452fe9fe1d618
Pablo Cavero Perez <gelves.mula@gmail.com>
2021-01-27 20:20:22 +0100

    Falta eliminar una entrada de la agenda

--- P5/Principal.py
+++ P5/Principal.py
@@ -2,32 +2,18 @@ from Model.Agenda import Agenda
 from Model.Usuario import Usuario
 import linecache
 
-
-
-usuario = Usuario("jose","Blanco",959331728)
-
+usuario = Usuario("sara", "z", 959331728)
 
 x = Agenda()
 
+print("hola " + x.buscarTelefono(usuario) + "hola")
 
-print("hola "+x.buscarTelefono(usuario)+"hola")
+archivo = open("file.txt", "r+")
 
-
-
-archivo = open("file.txt","r+")
-
-
-#with open("file.txt", "r") as f:
+# with open("file.txt", "r") as f:
 #    lines = f.readlines()
-#with open("file.txt", "w") as f:
+# with open("file.txt", "w") as f:
 #    print(len(f.readlines()))
 #    for line in lines:
 #        if line.strip("\n") != "Nombre: jose ":
 #            f.write(line)
-
-
-
-
-
-
-
